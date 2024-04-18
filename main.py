e2f2532952fe2357b43d9836ef209c6e6ed1f5fa
mikecode124 <michaelkimmail@gmail.com>
2024-04-18 00:16:44 -0700

    docblocks for tasks #1-6 complete

--- main.py
+++ main.py
@@ -19,6 +19,7 @@
 
 import pandas as pd
 
+
 # --> Methods:
 
 def sample(sample):
@@ -28,68 +29,55 @@ def sample(sample):
 
     return sample
 
+
 # --> Main:
 
 # -----------------------------------------------------------------------------
 # Task #1:
-# - Loops restricted / List comprehension structures restricted
-# - Write 'percent_of_mean()'
-#   - Input is a 1-D array of numeric values
-#       - values represent the stock index for each day as a % of the
-#         mean value over the period covered by the list
-#   - Find mean of all values
-#   - Store representation of each value as a % of the mean
+# - Read in the data file and save its contents as a DataFrame object
+#       -Label the columns appropriately
 # -----------------------------------------------------------------------------
 
 
 col_labels = ["name", "landmass", "zone", "area", "population", "language",
-                  "religion", "bars", "stripes", "colours", "red", "green", "blue",
-                  "gold", "white", "black", "orange", "mainhue", "circles", "crosses",
-                  "saltires", "quarters", "sunstar", "crescent", "triangle", "icon",
-                  "animate", "text", "topleft", "botright"]
+              "religion", "bars", "stripes", "colours", "red", "green", "blue",
+              "gold", "white", "black", "orange", "mainhue", "circles", "crosses",
+              "saltires", "quarters", "sunstar", "crescent", "triangle", "icon",
+              "animate", "text", "topleft", "botright"]
 
-#print("number of column labels: " + str(len(col_labels)))
+# print("number of column labels: " + str(len(col_labels)))
 
 col_labels_s = pd.Series(col_labels)
 
 with open("flag.data", "r") as flagfile:
     df0 = pd.read_csv(flagfile, header=None)
-    #print("rows, cols of dataset: " + str(np.shape(df0)))
-    #print(df0.dtypes)
+    # print("rows, cols of dataset: " + str(np.shape(df0)))
+    # print(df0.dtypes)
 
 df1 = df0.copy()
 df1.columns = col_labels_s.astype(object)
-#print(df1.head())
+# print(df1.head())
 
 # -----------------------------------------------------------------------------
 # Task #2:
-# - Loops restricted / List comprehension structures restricted
-# - Write 'percent_of_mean()'
-#   - Input is a 1-D array of numeric values
-#       - values represent the stock index for each day as a % of the
-#         mean value over the period covered by the list
-#   - Find mean of all values
-#   - Store representation of each value as a % of the mean
+# - Print out how many countries in the dataset are in North America
 # -----------------------------------------------------------------------------
 
 
-#print(df1.groupby("landmass").size())
+# print(df1.groupby("landmass").size())
 # or more specifically
 print("Number in N. America: " + str(df1.groupby("landmass").size().get(1))
-        + "\n")  # 31
+      + "\n")  # 31
 
 # -----------------------------------------------------------------------------
 # Task #3:
-# - Loops restricted / List comprehension structures restricted
-# - Write 'percent_of_mean()'
-#   - Input is a 1-D array of numeric values
-#       - values represent the stock index for each day as a % of the
-#         mean value over the period covered by the list
-#   - Find mean of all values
-#   - Store representation of each value as a % of the mean
+# - Print out how many countries are in each of the landmasses
+# - Do this once using explicit loops to go through the data items
+#   (i.e., without using the testing and aggregation capabilities of DataFrames)
+#       and once without using explicit loops
 # -----------------------------------------------------------------------------
 
-#print(len(df1.groupby("landmass").size()))
+# print(len(df1.groupby("landmass").size()))
 print("Using explicit loops:")
 for x in range(1, len(df1.groupby("landmass").size()) + 1):
     print(str(x) + "\t " + str(df1.groupby("landmass").size().get(x)))
@@ -100,13 +88,12 @@ print("Without using explicit loops:\n" + str(df1.groupby("landmass").size())
 
 # -----------------------------------------------------------------------------
 # Task #4:
-# - Loops restricted / List comprehension structures restricted
-# - Write 'percent_of_mean()'
-#   - Input is a 1-D array of numeric values
-#       - values represent the stock index for each day as a % of the
-#         mean value over the period covered by the list
-#   - Find mean of all values
-#   - Store representation of each value as a % of the mean
+# - Print out the total population (in millions) of the countries that speaks
+#   each language
+# - Print out the total population (in millions) of the countries that speak
+#   each language whose national populations are less than 50 million
+# - In both cases, print in decreasing order of total population
+# - Any conclusions you can make from the different charts?
 # -----------------------------------------------------------------------------
 
 
@@ -116,52 +103,54 @@ print("\n")
 
 pop_sub50 = df1["population"] < 50
 allrows_pop_sub50 = df1[pop_sub50]
-#print(allrows_pop_sub50)
-#print("\n")
+# print(allrows_pop_sub50)
+# print("\n")
 
 lang_pop_sub50 = allrows_pop_sub50.groupby("language")["population"].sum().sort_values(ascending=False)
 print(lang_pop_sub50)
 print("\n")
+'''
+Any conclusions you can make from the different charts?
+-English is not spoken in THAT many countries.
+-The majority of countries with populations below 50 million
+    do not speak any of the major languages on the list and 
+    must speak their own indigenous languages.
+'''
 
 # -----------------------------------------------------------------------------
 # Task #5:
-# - Loops restricted / List comprehension structures restricted
-# - Write 'percent_of_mean()'
-#   - Input is a 1-D array of numeric values
-#       - values represent the stock index for each day as a % of the
-#         mean value over the period covered by the list
-#   - Find mean of all values
-#   - Store representation of each value as a % of the mean
+# - Create a merged DataFrame object that is the intersection of the flags
+#   dataset and the given employee dataset
+# - Print out the total number of "representative-countries" the sales team
+#   covers based on language spoken
+#   - Each country whose language a sales representative can speak counts
+#       as a "representative-country"
 # -----------------------------------------------------------------------------
 
 
 country_lang = df1.loc[:, "language"]
-#print(country_lang)
-#print("\n")
+# print(country_lang)
+# print("\n")
 
 rep_lang = {"rep name": ["Max", "Jill", "Fong", "Juanita", "Nya"],
             "language": [1, 2, 5, 5, 8]}
-#print(rep_lang)
-#print("\n")
+# print(rep_lang)
+# print("\n")
 rep_df = pd.DataFrame(rep_lang)
-#print(rep_df)
-#print("\n")
+# print(rep_df)
+# print("\n")
 
-#country_rep_lang = pd.merge(country_lang, rep_df, left_on="rep lang", right_on="language")
+# country_rep_lang = pd.merge(country_lang, rep_df, left_on="rep lang", right_on="language")
 # rather than use left/right_on parameters I renamed the dictionary to have a matching label
 country_rep_lang = pd.merge(country_lang, rep_df)
-print("Total representative-countries: " + str(len(country_rep_lang)))  #91
+print("Total representative-countries: " + str(len(country_rep_lang)))  # 91
 print("\n")
 
 # -----------------------------------------------------------------------------
 # Task #6:
-# - Loops restricted / List comprehension structures restricted
-# - Write 'percent_of_mean()'
-#   - Input is a 1-D array of numeric values
-#       - values represent the stock index for each day as a % of the
-#         mean value over the period covered by the list
-#   - Find mean of all values
-#   - Store representation of each value as a % of the mean
+# - Print a table showing the total area of countries for each possible
+#   combination of landmass and language.
+# - What do the NaN values mean in that table?
 # -----------------------------------------------------------------------------
 
 # groupby() way:
@@ -173,9 +162,10 @@ landmass_lang_area_pt = df1.pivot_table(index="landmass", columns="language",
 
 print(landmass_lang_area_pt)
 '''
-NaN represent combinations of landmass and language codes which do not exist within 
-the dataset. Or in other words, that the selected languages are not spoken inside 
-the specified land masses.
+What do the NaN values mean in that table?
+-NaN represent combinations of landmass and language codes which do not exist within 
+    the dataset. Or in other words, that the selected languages are not spoken inside 
+    the specified land masses.
 '''
 
-# End of File
+# End of File T-T #
